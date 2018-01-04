d73ff69933730b6dbc4084f964861b7201a6be5b
Fumito Hamamura <fumito.ham@gmail.com>
2018-01-04 12:19:09 +0900

    DIST: Bump revision number

--- lifelib/__init__.py
+++ lifelib/__init__.py
@@ -1,6 +1,6 @@
 import os.path
 
-VERSION = (0, 0, 1)
+VERSION = (0, 0, 2)
 LIB_DIR = os.path.abspath(os.path.dirname(__file__))
 TEMPLATE_DIR = os.path.join(LIB_DIR, 'projects')
 TEMPLATES = [f for f in os.listdir(TEMPLATE_DIR)
