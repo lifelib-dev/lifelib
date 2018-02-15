7aedf086ba05ee0847bcc8628226b74f295c2f56
Fumito Hamamura <fumito.ham@gmail.com>
2018-02-16 00:00:56 +0900

    DIST: Update long description for PyPI

--- setup.py
+++ setup.py
@@ -26,8 +26,51 @@ here = path.abspath(path.dirname(__file__))
 #     LONG_DESCRIPTION = f.read()
 
 LONG_DESCRIPTION = """
-lifelib is a Python package of practical life actuarial models
-written in Python.
+**lifelib** is a collection of actuarial projection models.
+lifelib models are built as `modelx`_ models, ready to be used out of the box
+with sample formulas and input files, and they are
+fully customizable by users.
+
+.. _modelx: http://docs.modelx.io
+
+Feature highlights
+------------------
+
+    - Formulas and their calculated values paired as **Cells**,
+      just like spreadsheet cells
+    - Relevant cells grouped together as a **Space**, just like a spreadsheet
+    - Spaces in other spaces (subspaces), forming trees of spaces
+    - **Models** composed of spaces
+    - Space inheritance
+    - Parametrized dynamic subspaces created automatically
+    - Saving to / loading from files
+    - Conversion to Pandas objects
+    - Reading data from Excel files
+    - Cells graph to track cells interdependency
+
+Why **lifelib**?
+----------------
+
+    - Better model integrity and extensibility
+    - For readable formula expressions
+    - For eliminating spreadsheet errors
+    - For better version control/model governance
+
+What for?
+---------
+
+    - Pricing / Profit testing
+    - Model validation / testing
+    - Prototyping for production models
+    - As corporate models
+    - For simulations
+    - As replacement for any spreadsheet models
+
+Related sites
+-------------
+
+    - **lifelib** development site: https://github.com/fumitoh/lifelib
+    - **modelx** documentation: http://docs.modelx.io
 """
 
 def get_version(version_tuple):
