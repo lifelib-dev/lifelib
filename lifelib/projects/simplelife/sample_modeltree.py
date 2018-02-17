acf0eab6799440646aeb6627b22342224e402df5
Fumito Hamamura <fumito.ham@gmail.com>
2018-02-18 00:24:38 +0900

    FIX: Make model tree work on Spyder

--- lifelib/projects/simplelife/sample_modeltree.py
+++ lifelib/projects/simplelife/sample_modeltree.py
@@ -1,5 +1,15 @@
-from modelx.qtgui import *
+import sys
+from modelx.qtgui import get_modeltree
+from qtpy.QtWidgets import QApplication
+
 import simplelife
 
 if __name__ == "__main__":
-    show_modeltree(simplelife.build(load_saved=False))
+
+    app = QApplication.instance()
+    if not app:
+        app = QApplication(sys.argv)
+
+    view = get_modeltree(simplelife.build(load_saved=False))
+    view.show()
+    app.exec_()     # Not needed for Spyder
