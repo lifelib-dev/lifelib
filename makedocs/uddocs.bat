e52c81dfe02ae7df70d29d2ae27cd615c6a69ad8
Fumito Hamamura <fumito.ham@gmail.com>
2018-04-01 23:11:00 +0900

    DOC: Update uddocs.bat to build site in lifelib-docs repo

--- makedocs/uddocs.bat
+++ makedocs/uddocs.bat
@@ -1,4 +1,4 @@
 :: Remove a constant path of my PC.
 python uddocs.py
 robocopy uddocs build\html * >uddocs.log
-robocopy build\html ..\docs /mir >> uddocs.log
+robocopy build\html ..\..\lifelib-docs\docs /mir >> uddocs.log
