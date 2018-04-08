4d80ed20f1905e0b5760e6b375596cb1493edbf6
Fumito Hamamura <fumito.ham@gmail.com>
2018-04-08 13:05:53 +0900

    DOC: Add past docs under archive page

---
+++ makedocs/source/archive.rst
@@ -0,0 +1,12 @@
+Archive
+=======
+
+
+Past Releases
+-------------
+
+.. raw:: html
+
+    <ul class="simple">
+    <li><a href="archive/docs-v0.0.5/index.html">lifelib v0.0.5</a></li>
+    </ul>
--- makedocs/uddocs.bat
+++ makedocs/uddocs.bat
@@ -1,4 +1,5 @@
 :: Remove a constant path of my PC.
 python uddocs.py
 robocopy uddocs build\html * >uddocs.log
+robocopy ..\..\lifelib-docs\archive build\html\archive /mir >> uddocs.log
 robocopy build\html ..\..\lifelib-docs\docs /mir >> uddocs.log
