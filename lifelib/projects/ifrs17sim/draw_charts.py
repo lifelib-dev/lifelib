19fa0713d9019f83ff4045f3d0500bd220e55d17
Fumito Hamamura <fumito.ham@gmail.com>
2018-05-26 12:47:32 +0900

    ENH: Add WaterfallColorPalette

--- lifelib/projects/ifrs17sim/draw_charts.py
+++ lifelib/projects/ifrs17sim/draw_charts.py
@@ -4,9 +4,10 @@
 
 Draw a graph of CSM amortization pattern.
 """
-import math
+
 import collections
 import pandas as pd
+import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
 
@@ -15,19 +16,63 @@ def draw_waterfall(df, ax=None):
     """Draw waterfall chart"""
     data = df.stack()
     bottom = df.cumsum(axis=1).shift(1, axis=1).fillna(0).stack()
-    flowvals= set(df.iloc[:, 1:].stack())
-    colors = CustomColorMap(flowvals)
+    palette = WaterfallColorPalette(df)
     xlabel = [idx[1] + '(' + str(idx[0]) + ')' for idx in df.stack().index]
     ax = sns.barplot(data=data, bottom=bottom,
-                     palette=colors.get_palette(data),
+                     palette=palette.colors,
                      ax=ax)
     ax.set_xticklabels(labels=xlabel, rotation='vertical')
     ax.get_figure().tight_layout()
     return ax
 
 
+class WaterfallColorPalette:
+    """Returns color palette for drawing waterfall bars.
+
+    The length of `data` columns
+
+    Args:
+        data: a DataFrame of waterfall data.
+        length: the number of bars in an interval. Defaults to the length of
+                `data` columns.
+    """
+    
+    def __init__(self, data, length=None):
+
+        self.data = data
+        if length is None:
+            self.length = len(data.columns)
+        else:
+            self.length = length
+
+        self.paired = plt.get_cmap('Paired').colors
+        self.colors = self.set_colors()
+
+    def set_colors(self):
+
+        result = []
+        row_count, _ = self.data.shape
+
+        for row in range(row_count):
+            for col in range(self.length):
+                sign = self.data.iloc[row, col] >= 0
+                result.append(self.get_color(col, sign))
+
+        return result
+
+    def get_color(self, idx, sign):
+        pair_count = len(self.paired) // 2
+        idx = 2 * (idx % pair_count)
+        return self.paired[idx + 1] if sign else self.paired[idx]
+
+
 class CustomColorMap:
-    """Change color by size and sign"""
+    """Unused. Colors changing by size and sign of data
+
+    Args:
+        data: a sequence of bar values.
+    """
+    import math
 
     def __init__(self, data):
 
@@ -50,7 +95,7 @@ class CustomColorMap:
             return min(idx0 + math.floor(idx0 * value / absmax),
                        self.pallen - 1)
 
-    def get_palette(self, data):
+    def get_colors(self, data):
         idxs = [int(self.get_index(val)) for val in data]
         return [self.palette[i] for i in idxs]
 
@@ -76,3 +121,4 @@ if __name__ == '__main__':
     df['TransServices'] = -1 * df['TransServices']
 
     draw_waterfall(df)
+    plt.show()
