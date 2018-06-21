9d1bd0cf9db8aa948a2ff215031083c2513e1a4e
Fumito Hamamura <fumito.ham@gmail.com>
2018-06-21 21:19:35 +0900

    DOC: Change archive location

--- makedocs/uddocs.bat
+++ makedocs/uddocs.bat
@@ -1,5 +1,4 @@
 :: Remove a constant path of my PC.
 python uddocs.py
 robocopy uddocs build\html * >uddocs.log
-robocopy ..\..\lifelib-docs\archive build\html\archive /mir >> uddocs.log
 robocopy build\html ..\..\lifelib-docs\docs /mir >> uddocs.log
