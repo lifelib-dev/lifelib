cec057489015787735a77a36814e3cca418aa6f0
Fumito Hamamura <fumito.ham@gmail.com>
2020-04-18 14:07:37 +0900

    FIX: Sphinx-gallery error due to its update

--- makedocs/source/conf.py
+++ makedocs/source/conf.py
@@ -273,7 +273,7 @@ sphinx_gallery_conf = {
     # path where to save gallery generated examples
     'gallery_dirs': 'generated_examples',
     # Suppress warning:
-    'backreferences_dir': False,
+    'backreferences_dir': None,
     'download_all_examples': False,
     'download_section_examples': False,
     'capture_repr': ()
