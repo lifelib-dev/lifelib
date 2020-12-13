608f2d793cc84c070b2185080eb294a57b5a8e37
Fumito Hamamura <fumitoh@users.noreply.github.com>
2020-12-13 13:47:24 +0900

    DIST: Add Python 3.8 and 3.9 in setup.py

--- setup.py
+++ setup.py
@@ -133,6 +133,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 
     # What does your project relate to?
