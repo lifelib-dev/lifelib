57a26557256d992773bd2ace01dd555a55223c8f
Fumito Hamamura <fumito.ham@gmail.com>
2021-02-14 01:14:02 +0900

    DIST: Release v0.0.16

--- lifelib/__init__.py
+++ lifelib/__init__.py
@@ -2,7 +2,7 @@ import os.path
 from lifelib._dirs import TEMPLATE_DIR, TEMPLATES
 from lifelib.commands.create import create
 
-VERSION = (0, 0, 15)
+VERSION = (0, 0, 16)
 __version__ = '.'.join([str(x) for x in VERSION])
 
 
--- setup.py
+++ setup.py
@@ -152,7 +152,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['modelx>=0.11.0'],
+    install_requires=['modelx>=0.12.1'],
 
     # If your project only runs on certain Python versions,
     # setting the python_requires argument to the appropriate PEP 440 version
