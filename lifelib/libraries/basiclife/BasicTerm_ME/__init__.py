b787f43ddfd1d0eeac7e39f124ea49369c2445de
Fumito Hamamura <fumito.ham@gmail.com>
2021-08-07 20:55:27 +0900

    ENH: Add BasicTerm_SE and BasicTerm_ME

---
+++ lifelib/libraries/basiclife/BasicTerm_ME/__init__.py
@@ -0,0 +1,10 @@
+from modelx.serialize.jsonvalues import *
+
+_name = "BasicTerm_ME"
+
+_allow_none = False
+
+_spaces = [
+    "Projection"
+]
+
---
+++ lifelib/libraries/basiclife/BasicTerm_SE/__init__.py
@@ -0,0 +1,10 @@
+from modelx.serialize.jsonvalues import *
+
+_name = "BasicTerm_SE"
+
+_allow_none = False
+
+_spaces = [
+    "Projection"
+]
+
