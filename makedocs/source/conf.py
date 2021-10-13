5496315f8bf1e15ff0f0ea7a9cd334d985dd3e58
Fumito Hamamura <fumito.ham@gmail.com>
2021-10-13 20:41:05 +0900

    DOC: Add html title

--- makedocs/source/conf.py
+++ makedocs/source/conf.py
@@ -106,6 +106,8 @@ todo_include_todos = True
 html_theme = "pydata_sphinx_theme"
 html_logo = "images/logo.png"
 
+html_title = "lifelib " + version
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
