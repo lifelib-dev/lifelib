ee4e754f4230b8bfeba50b18cd61ac6109ed9572
Fumito Hamamura <fumito.ham@gmail.com>
2025-02-01 11:30:42 +0900

    DIST: Update setup.py to include Python 3.13

--- setup.py
+++ setup.py
@@ -161,7 +161,8 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12'
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13'
     ],
 
     # What does your project relate to?
